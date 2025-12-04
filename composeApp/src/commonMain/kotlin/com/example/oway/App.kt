17e4384bebb08932fd2b9e8aa26fd5f056c46635
soekhant01 <soekhant.dev@gmail.com>
2025-12-04 22:14:30 +0630

    RoleScreen implemented

--- composeApp/src/commonMain/kotlin/com/example/oway/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/oway/App.kt
@@ -9,10 +9,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.example.oway.ui.screens.AuthScreen
 import com.example.oway.ui.theme.OwayAppTheme
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -23,5 +25,9 @@ import oway.composeapp.generated.resources.compose_multiplatform
 @Composable
 @Preview
 fun App() {
-    OwayAppTheme{}
+    OwayAppTheme{
+        Scaffold {
+            AuthScreen()
+        }
+    }
 }
---
+++ composeApp/src/commonMain/kotlin/com/example/oway/ui/screens/RoleScreen.kt
@@ -0,0 +1,97 @@
+package com.example.oway.ui.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+import oway.composeapp.generated.resources.Res
+import oway.composeapp.generated.resources.welcome
+
+@Composable
+fun RoleScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painterResource(Res.drawable.welcome),
+            contentDescription = null,
+            modifier = Modifier.fillMaxWidth()
+                .padding(start = 16.dp, top = 48.dp, end = 16.dp, bottom = 16.dp),
+            contentScale = ContentScale.Crop
+        )
+
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Welcome",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(
+                Modifier.height(8.dp)
+            )
+            Text(
+                text = "Have a better sharing experience",
+                color = Color(0xFFA0A0A0)
+            )
+        }
+
+        Column(
+            modifier = Modifier.fillMaxSize().padding(16.dp),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(
+                onClick = {},
+                modifier = Modifier.fillMaxWidth(),
+                shape = androidx.compose.foundation.shape.RoundedCornerShape(8.dp)
+
+            ) {
+                Text(
+                    text = "Create an account"
+                )
+            }
+
+            Spacer(Modifier.height(8.dp))
+
+            OutlinedButton(
+                onClick = {},
+                modifier = Modifier.fillMaxWidth(),
+                shape = androidx.compose.foundation.shape.RoundedCornerShape(8.dp)
+
+            ) {
+                Text(
+                    text = "Login"
+                )
+            }
+        }
+
+    }
+}
+
+@Composable
+@Preview
+fun AuthScreenPreview() {
+    RoleScreen()
+}
